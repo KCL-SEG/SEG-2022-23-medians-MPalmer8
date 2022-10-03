b1ef5a1cd917094c99084d71cbaaf1f008b9d61f
Matthew Palmer <matthewpalmer135@gmail.com>
2022-10-04 00:23:06 +0100

    Added median function

--- median.py
+++ median.py
@@ -1,5 +1,15 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+def medianfunc(numbers = []):
+    index = len(numbers)/2
+    num = int(index)
+    if len(numbers)%2:
+        median = numbers[num]
+        return median
+    else:
+        median = (numbers[num] + numbers[num - 1])/2
+        return median
+
 
 while True:
     try:
@@ -9,4 +19,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(medianfunc(numbers))
